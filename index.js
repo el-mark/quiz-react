501ba435b00851f6709736dd7883b7b94213604b
Mark Harmsen <mharmsenr@gmail.com>
2019-02-11 18:00:34 -0300

    React pre-install part 2

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Question from './src/question';
+
+const app = document.getElementById('app');
+
+render(<Question />, app);
---
+++ src/question.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// import './question.css';
+
+class Question extends Component {
+    render() {
+        return(
+            <div>Este es un div de Question</div>
+        )
+    }
+}
+
+export default Question;
