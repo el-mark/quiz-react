0840c1f1575dee9d87677706dd5628099c8ea746
Mark Harmsen <mharmsenr@gmail.com>
2019-02-12 10:41:35 -0300

    Add all the html structur and css to the react app. Plus add some properties to the question component.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Question from './src/question';
+import './index.css'
 
 const app = document.getElementById('app');
 
-render(<Question />, app);
+render(<Question questionNum='2/7' questionStatement='¿Te gusta leer?' />, app);
--- src/question.js
+++ src/question.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
-// import './question.css';
+import './question.css';
 
 class Question extends Component {
+    handleClick = (event) => {
+        console.log(event.target.getAttribute('value'))
+    }
     render() {
         return(
-            <div>Este es un div de Question</div>
+            <div>
+                <div className="question">
+                    <div>Pregunta {this.props.questionNum}</div>
+                    {this.props.questionStatement}
+                </div>
+                <div className="answer" onClick={this.handleClick} value="1">
+                    <div>1</div>
+                    Me encanta
+                </div>
+                <div className="answer" onClick={this.handleClick} value="2">
+                    <div>2</div>
+                    Me gusta
+                </div>
+                <div className="answer" onClick={this.handleClick} value="3">
+                    <div>3</div>
+                    Neutral
+                </div>
+                <div className="answer" onClick={this.handleClick} value="4">
+                    <div>4</div>
+                    No me gusta
+                </div>
+                <div className="answer" onClick={this.handleClick} value="5">
+                    <div>5</div>
+                    Lo detesto
+                </div>
+            </div>
         )
     }
 }
