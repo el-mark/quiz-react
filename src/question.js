a75ab4756ad83c52fa9f62f72b81195ec1b974a9
Mark Harmsen <mharmsenr@gmail.com>
2019-02-13 18:01:21 -0300

    Add results acumulator (app is broken)

--- src/question.js
+++ src/question.js
@@ -4,20 +4,43 @@ import './question.css';
 class Question extends Component {
     state = {
         questions: this.props.questions,
-        questionNum: 1,
+        questionNum: 0,
         questionTot: this.props.questions.length,
         questionId: this.props.questions[0].id,
         questionStatement: this.props.questions[0].statement,
-        results: {}
+        results: []
     }
     handleClick = (event) => {
-        console.log(event.target.getAttribute('value'))
+        const chosenValue = event.target.getAttribute('value');
+        const results = this.generateResults(chosenValue);
+
+        const questionNum = this.state.questionNum;
+        
+        this.setState({
+            results: results,
+            questionNum: this.state.questionNum + 1,
+            questionStatement: this.props.questions[questionNum].statement,
+            questionId: this.props.questions[questionNum].id,
+        })
+    }
+    generateResults = (chosenValue) => {
+        console.log('inside GenerateResults');
+        const thisResult = {
+            "id": this.state.questionId,
+            "statement": this.state.questionStatement,
+            "value": chosenValue
+        }
+        console.log('thisResult');
+        console.log(thisResult);
+        var results = this.state.results;
+        results.push(thisResult);
+        return results;
     }
     render() {
         return(
             <div>
                 <div className="question">
-                    <div>Pregunta {this.state.questionNum}/{this.state.questionTot}</div>
+                    <div>Pregunta {this.state.questionNum+1}/{this.state.questionTot}</div>
                     {this.state.questionStatement}
                 </div>
                 <div className="answer" onClick={this.handleClick} value="1">
