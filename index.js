26ea7e12909e39f9ce727a998907ddc85ea16da0
Mark Harmsen <mharmsenr@gmail.com>
2019-02-14 16:35:05 -0300

    Add statis pages and create start component

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Question from './src/question';
-import './index.css'
-import data from './src/api.json'
+import data from './src/api.json';
+import Home from './src/home/home'
 
 const app = document.getElementById('app');
 
-render(<Question questions={data.questions} />, app);
+render(<Home data={data} />, app);
